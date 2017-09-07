6eba0a32d7b448622b3e25353fe8a6092e58d0fd
honeyful <honeyful@users.noreply.github.com>
2017-09-07 22:13:24 +0900

    firstCommit

---
+++ MainFrame.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WFH
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkBlockAllInbound = new System.Windows.Forms.CheckBox();
+            this.chkBlockOutbound = new System.Windows.Forms.CheckBox();
+            this.btnHarden = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // chkBlockAllInbound
+            // 
+            this.chkBlockAllInbound.AutoSize = true;
+            this.chkBlockAllInbound.Checked = true;
+            this.chkBlockAllInbound.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBlockAllInbound.Location = new System.Drawing.Point(12, 201);
+            this.chkBlockAllInbound.Name = "chkBlockAllInbound";
+            this.chkBlockAllInbound.Size = new System.Drawing.Size(161, 16);
+            this.chkBlockAllInbound.TabIndex = 0;
+            this.chkBlockAllInbound.Text = "Block All Inbound Traffic";
+            this.chkBlockAllInbound.UseVisualStyleBackColor = true;
+            // 
+            // chkBlockOutbound
+            // 
+            this.chkBlockOutbound.AutoSize = true;
+            this.chkBlockOutbound.Checked = true;
+            this.chkBlockOutbound.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBlockOutbound.Location = new System.Drawing.Point(179, 201);
+            this.chkBlockOutbound.Name = "chkBlockOutbound";
+            this.chkBlockOutbound.Size = new System.Drawing.Size(152, 16);
+            this.chkBlockOutbound.TabIndex = 1;
+            this.chkBlockOutbound.Text = "Block Outbound Traffic";
+            this.chkBlockOutbound.UseVisualStyleBackColor = true;
+            // 
+            // btnHarden
+            // 
+            this.btnHarden.Location = new System.Drawing.Point(12, 252);
+            this.btnHarden.Name = "btnHarden";
+            this.btnHarden.Size = new System.Drawing.Size(636, 23);
+            this.btnHarden.TabIndex = 2;
+            this.btnHarden.Text = "Harden";
+            this.btnHarden.UseVisualStyleBackColor = true;
+            this.btnHarden.Click += new System.EventHandler(this.btnHarden_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 223);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(636, 23);
+            this.progressBar.TabIndex = 3;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 281);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(636, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 12);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(636, 183);
+            this.txtLog.TabIndex = 5;
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 316);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnHarden);
+            this.Controls.Add(this.chkBlockOutbound);
+            this.Controls.Add(this.chkBlockAllInbound);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainFrame";
+            this.Text = "Windows Firewall Harden";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkBlockAllInbound;
+        private System.Windows.Forms.CheckBox chkBlockOutbound;
+        private System.Windows.Forms.Button btnHarden;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ MainFrame.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System.Threading;
+using NetFwTypeLib;
+
+namespace WFH
+{
+    public partial class MainFrame : Form
+    {
+        private volatile bool _stop = false;
+
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void fwHarden()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            btnReset.Enabled = false;
+            btnHarden.Text = "Stop";
+
+            fwReset();
+
+            List<string> fileList = GetFilesRecursive("C:\\");
+            progressBar.Maximum = fileList.Count;
+
+            foreach (var file in fileList)
+            {
+                if (_stop) break;
+                if (checkSignature(file))
+                {
+                    allowRule(file);
+                    txtLog.AppendText($"[+]{file}" + Environment.NewLine);
+                }
+                else
+                {
+                    blockRule(file);
+                    txtLog.AppendText($"[-]{file}" + Environment.NewLine);
+                }
+                progressBar.Value += 1;
+            }
+
+            fwInitRule();
+
+            btnReset.Enabled = true;
+            btnHarden.Text = "Harden";
+            progressBar.Value = 0;
+            MessageBox.Show("Done.");
+            
+        }
+
+        private void allowRule(string fileName)
+        {
+            INetFwRule fwRule = (INetFwRule)Activator.CreateInstance(
+                 Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            fwRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+            fwRule.Enabled = true;
+            fwRule.InterfaceTypes = "All";
+            fwRule.Name = fileName;
+            fwRule.ApplicationName = fileName;
+            INetFwPolicy2 fwPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            fwRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            fwPolicy.Rules.Add(fwRule);
+        }
+
+        private void blockRule(string fileName)
+        {
+            INetFwRule fwRule = (INetFwRule)Activator.CreateInstance(
+                Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            fwRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+            fwRule.Enabled = true;
+            fwRule.InterfaceTypes = "All";
+            fwRule.Name = fileName;
+            fwRule.ApplicationName = fileName;
+            INetFwPolicy2 fwPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            fwRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            fwPolicy.Rules.Add(fwRule);
+        }
+
+        private void fwReset()
+        {
+            Type netFwPolicy2Type = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 mgr = (INetFwPolicy2)Activator.CreateInstance(netFwPolicy2Type);
+            mgr.RestoreLocalFirewallDefaults();
+        }
+
+        private void fwInitRule()
+        {
+            Type netFwPolicy2Type = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 mgr = (INetFwPolicy2)Activator.CreateInstance(netFwPolicy2Type);
+
+            mgr.set_BlockAllInboundTraffic(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN, chkBlockAllInbound.Checked ? true : false);
+            mgr.set_DefaultInboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+            mgr.set_DefaultOutboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN, !chkBlockOutbound.Checked ? NET_FW_ACTION_.NET_FW_ACTION_ALLOW : NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+
+            mgr.set_BlockAllInboundTraffic(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE, chkBlockAllInbound.Checked ? true : false);
+            mgr.set_DefaultInboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+            mgr.set_DefaultOutboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE, !chkBlockOutbound.Checked ? NET_FW_ACTION_.NET_FW_ACTION_ALLOW : NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+
+            mgr.set_BlockAllInboundTraffic(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC, chkBlockAllInbound.Checked ? true : false);
+            mgr.set_DefaultInboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+            mgr.set_DefaultOutboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC, !chkBlockOutbound.Checked ? NET_FW_ACTION_.NET_FW_ACTION_ALLOW : NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+        }
+
+        private bool checkSignature(string fileName)
+        {
+            var certChain = new X509Chain();
+            var cert = default(X509Certificate2);
+            bool isChainValid = false;
+            try
+            {
+                var signer = X509Certificate.CreateFromSignedFile(fileName);
+                cert = new X509Certificate2(signer);
+            }
+            catch
+            {
+                return isChainValid;
+            }
+
+            certChain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+            certChain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+            certChain.ChainPolicy.UrlRetrieval‎Timeout = new TimeSpan(0, 1, 0);
+            certChain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+            isChainValid = certChain.Build(cert);
+
+            return isChainValid;
+        }
+
+
+        private List<string> GetFilesRecursive(string initial)
+        {
+            List<string> result = new List<string>();
+
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push(initial);
+
+            while ((stack.Count > 0))
+            {
+                string dir = stack.Pop();
+                try
+                {
+                    result.AddRange(Directory.GetFiles(dir, "*.exe"));
+
+                    string directoryName = null;
+                    foreach (string directoryName_loopVariable in Directory.GetDirectories(dir))
+                    {
+                        directoryName = directoryName_loopVariable;
+                        stack.Push(directoryName);
+                    }
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            return result;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            fwReset();
+            MessageBox.Show("Reset");
+        }
+
+        private void btnHarden_Click(object sender, EventArgs e)
+        {
+            if(btnHarden.Text == "Harden")
+            {
+                _stop = false;
+                new Thread(fwHarden).Start();
+            }
+            else
+            {
+                _stop = true;
+            }
+        }
+    }
+}
